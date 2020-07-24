3c97fc38edfbc8756a085a6db1fc1d2d2368ef88
diegosrtrainninggit <diegosr.trainning.git@gmail.com>
2020-07-24 05:37:18 -0300

    Mock /informacoes_recebidas/macros_enviadas_motorista 2

--- controllers/informacoes_recebidas/macrosController.js
+++ controllers/informacoes_recebidas/macrosController.js
@@ -6,58 +6,20 @@ require('dotenv').config();
 // Lista de atuadores
 exports.list = function(req, res, next) {    
     let atuadores = {
-        "data": {
-          "atuadores": [
-            {
-              "codigo": 6,
-              "descricao": "ALARME (SIRENE)"
-            },
-            {
-              "codigo": 3,
-              "descricao": "BLOQUEIO"
-            },            
-            {
-              "codigo": 10,
-              "descricao": "BOTÃO DE PÂNICO"
-            },
-            {
-              "codigo": 2,
-              "descricao": "DESENGATE DE CARRETA"
-            },
-            {
-              "codigo": 12,
-              "descricao": "SENSOR DE BAÚ"
-            },
-            {
-              "codigo": 11,
-              "descricao": "SENSOR DE IGNIÇÃO"
-            },
-            {
-              "codigo": 8,
-              "descricao": "SENSOR DE PAINEL"
-            },
-            {
-              "codigo": 4,
-              "descricao": "SENSOR DE PORTA DO CARONA"
-            },
-            {
-              "codigo": 7,
-              "descricao": "SENSOR DE PORTA DO MOTORISTA"
-            },
-            {
-              "codigo": 9,
-              "descricao": "TECLADO DE COMUNICAÇÃO"
-            },
-            {
-              "codigo": 5,
-              "descricao": "TRAVA DA 5ª RODA"
-            },
-            {
-              "codigo": 5,
-              "descricao": "TRAVA DE BAU"
-            }
-          ] 
-        },
+      "data": {
+        "macros": [
+          {
+            "hora": "10:00",
+            "posicao": "1.222, 3.4444",
+            "macro": "Lorem ipsun"
+          },
+          {
+            "hora": "12:00",
+            "posicao": "2.11, 4.555",
+            "macro": "Lorem ipsun"
+          }
+        ]
+      },
         "message": {},
         "paging": {
           "finder": "string",

6cf992abacd37337a8a3e72e0f5fe7c671861bf6
diegosrtrainninggit <diegosr.trainning.git@gmail.com>
2020-08-19 13:36:57 -0300

    Adicionando rotas para viagens 2

--- controllers/viagens/viagensController.js
+++ controllers/viagens/viagensController.js
@@ -9,31 +9,21 @@ exports.find = function(req, res, next) {
         "data": {
             "viagens": [
                 {
-                  "sm": 23670669,
-                  "placa": "LLQ-9562",
-                  "situacao": "normal",
-                  "pedido": 951673,
-                  "status": "em_viagem",
-                  "alertas": {
+                    "sm": 23670669,
+                    "placa": "LLQ-9562",
+                    "situacao": "normal",
+                    "pedido": 951673,
+                    "status": "em_viagem",
+                    "alertas": {
                     "resolvidos": 0,
                     "total": 2
-                  },
-                  "mensagens": 10,
-                  "transportador": {                    
+                    },
+                    "mensagens": 10,
+                    "transportador": {                    
                     "codigo": 10,
                     "nome": "EXPRESSO LOCATADA LTDA - ME"
-                  },
-                  "origem": {
-                    "cidade": {
-                      "codigo": 1,
-                      "nome": "São Paulo"
                     },
-                    "uf": {
-                      "codigo": 10,
-                      "nome": "SP"
-                    }
-                  },
-                  "destino": {
+                    "origem": {
                     "cidade": {
                         "codigo": 1,
                         "nome": "São Paulo"
@@ -42,16 +32,26 @@ exports.find = function(req, res, next) {
                         "codigo": 10,
                         "nome": "SP"
                     }
-                  },
-                  "itinerarios": [
+                    },
+                    "destino": {
+                    "cidade": {
+                        "codigo": 1,
+                        "nome": "São Paulo"
+                    },
+                    "uf": {
+                        "codigo": 10,
+                        "nome": "SP"
+                    }
+                    },
+                    "itinerarios": [
                     {
-                      "local": "CLAUDIA ROQUETE PINTO",
-                      "entrada_prevista": "21/07/2020 10:00:00",
-                      "saida_prevista": "21/07/2020 10:30:00",
-                      "entrada_real": "21/07/2020 10:05:00",
-                      "saida_real": "21/07/2020 10:20:00",
-                      "permanencia": 15,
-                      "status": "adiantou"
+                        "local": "CLAUDIA ROQUETE PINTO",
+                        "entrada_prevista": "21/07/2020 10:00:00",
+                        "saida_prevista": "21/07/2020 10:30:00",
+                        "entrada_real": "21/07/2020 10:05:00",
+                        "saida_real": "21/07/2020 10:20:00",
+                        "permanencia": 15,
+                        "status": "adiantou"
                     },
                     {
                         "local": "RODRIGO ADELINO",
@@ -80,7 +80,7 @@ exports.find = function(req, res, next) {
                         "permanencia": 0,
                         "status": "recusou"
                     },
-                  ]
+                    ]
                 },
                 {
                     "sm": 23670669,
@@ -89,23 +89,23 @@ exports.find = function(req, res, next) {
                     "pedido": 951673,
                     "status": "em_viagem",
                     "alertas": {
-                      "resolvidos": 0,
-                      "total": 2
+                        "resolvidos": 0,
+                        "total": 2
                     },
                     "mensagens": 10,
                     "transportador": {                    
-                      "codigo": 11,
-                      "nome": "SANTOS MUNIZ TRANSPORTE RODOVIÁRIO DE CARGAS LTDA"
+                        "codigo": 11,
+                        "nome": "SANTOS MUNIZ TRANSPORTE RODOVIÁRIO DE CARGAS LTDA"
                     },
                     "origem": {
-                      "cidade": {
+                        "cidade": {
                         "codigo": 2,
                         "nome": "BRASILIA"
-                      },
-                      "uf": {
+                        },
+                        "uf": {
                         "codigo": 2,
                         "nome": "DF"
-                      }
+                        }
                     },
                     "destino": {
                         "cidade": {
@@ -118,7 +118,7 @@ exports.find = function(req, res, next) {
                         }
                     },
                     "itinerarios": [
-                      {
+                        {
                         "local": "RONILDA NOGUEIRA FRANCA",
                         "entrada_prevista": "20/08/2020 10:00:00",
                         "saida_prevista": "20/08/2020 10:30:00",
@@ -126,8 +126,8 @@ exports.find = function(req, res, next) {
                         "saida_real": "",
                         "permanencia": 0,
                         "status": "nao_entregou"
-                      },
-                      {
+                        },
+                        {
                         "local": "VANESSA RODRIGUES",
                         "entrada_prevista": "20/08/2020 11:00:00",
                         "saida_prevista": "20/08/2020 11:40:00",
@@ -135,35 +135,34 @@ exports.find = function(req, res, next) {
                         "saida_real": "",
                         "permanencia": 0,
                         "status": "nao_entregou"
-                      }
+                        }
                     ]
-                  }
-              ]
+                }
+                ]
             },
             "message": {},
             "paging": {
-              "finder": "string",
-              "page": 0,
-              "current": 0,
-              "count": 0,
-              "perPage": 0,
-              "start": 0,
-              "end": 0,
-              "prevPage": true,
-              "nextPage": true,
-              "pageCount": 0,
-              "sort": "string",
-              "direction": "string",
-              "limit": 0,
-              "sortDefault": true,
-              "directionDefault": true,
-              "scope": "string",
-              "completeSort": [
+                "finder": "string",
+                "page": 0,
+                "current": 0,
+                "count": 0,
+                "perPage": 0,
+                "start": 0,
+                "end": 0,
+                "prevPage": true,
+                "nextPage": true,
+                "pageCount": 0,
+                "sort": "string",
+                "direction": "string",
+                "limit": 0,
+                "sortDefault": true,
+                "directionDefault": true,
+                "scope": "string",
+                "completeSort": [
                 null
-              ]
-            }        
-        }    
-    }      
-
-    res.end(JSON.stringify(viagens));
+                ]
+            }                    
+        }      
+        
+        res.end(JSON.stringify(viagens));
 };    

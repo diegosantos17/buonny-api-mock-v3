57e1903ab0f865c6ab91b6a941766e8c686a04cc
diegosrtrainninggit <diegosr.trainning.git@gmail.com>
2020-08-19 14:33:47 -0300

    Ajustando endpoint de alertas

--- routes/viagens/viagens.js
+++ routes/viagens/viagens.js
@@ -6,5 +6,6 @@ var viagens_controller = require('../../controllers/viagens/viagensController');
 // GET catalog home page.
 router.get('/', viagens_controller.find);
 router.get('/filtros', viagens_controller.findFiltros);
+router.get('/alertas', viagens_controller.findAlertas);
 
 module.exports = router;
